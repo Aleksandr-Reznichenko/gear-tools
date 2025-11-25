21366dc6f1d95f1e5d5efe5b63af56b573c41be4
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2025-11-25 20:41:26 +0200

    add the current page attribute

---
+++ js/current-page.js
@@ -0,0 +1,21 @@
+(() => {
+  const currentPath = window.location.pathname.split('/').pop() || 'index.html';
+  const links = document.querySelectorAll('a[href]');
+
+  links.forEach(link => {
+    const href = link.getAttribute('href');
+
+    if (!href || href.startsWith('http') || href.startsWith('#')) {
+      return;
+    }
+
+    const normalizedHref = href.replace('./', '');
+
+    if (normalizedHref === currentPath) {
+      link.setAttribute('aria-current', 'page');
+      link.removeAttribute('href');
+    } else {
+      link.removeAttribute('aria-current');
+    }
+  });
+})();

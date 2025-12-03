b3c7c4c9a998834371b7e4eaf903ca56fd1e52f0
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2025-12-03 16:35:58 +0200

    made changes

--- js/scroll-page.js
+++ js/scroll-page.js
@@ -2,16 +2,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
 
-    const targetId = this.getAttribute('href').substring(1); // Отримуємо ідентифікатор цільового розділу
+    const targetId = this.getAttribute('href').substring(1);
 
-    // Знаходимо елемент цільового розділу
     const targetSection = document.getElementById(targetId);
 
     if (targetSection) {
-      // Виконуємо плавний скрол до цільового розділу
       window.scrollTo({
         top: targetSection.offsetTop,
-        behavior: 'smooth', // Додаємо плавність
+        behavior: 'smooth',
       });
     }
   });
